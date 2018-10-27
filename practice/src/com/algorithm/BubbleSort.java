fcf73ca464285a6f3cf0c7c867363041f736e058
857796028@qq.com <ZLBX7VJNchGIT>
2018-10-27 14:33:02 +0800

    ss

--- practice/src/com/algorithm/BubbleSort.java
+++ practice/src/com/algorithm/BubbleSort.java
@@ -9,13 +9,24 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
     int[] array = new int[]{9,8,7,4,3,0,1};
-        System.out.println(Arrays.toString(array));
-
+        System.out.println("排序之前--》 "+Arrays.toString(array));
+        bubbleSort(array);
+        System.out.println("排序之后--》 "+Arrays.toString(array));
 
 
     }
   public static void bubbleSort(int[] array){
+      for (int i = 0; i < array.length-1; i++) {
+          for (int j = 0; j < array.length-i-1; j++) {
+              if (array [j]>array[j+1]) {
+                  int temp = array[j];
+                  array[j] = array[j+1];
+                  array[j+1]=temp;
+              }
 
+          }
+
+      }
 
 
   }
