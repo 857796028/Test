7e5a0909d3cda2eee5f22ba4559eddb6ce40821e
857796028@qq.com <ZLBX7VJNchGIT>
2018-10-27 14:24:15 +0800

    ss

--- practice/src/com/algorithm/SortTest.java
+++ practice/src/com/algorithm/SortTest.java
@@ -1,7 +1,22 @@
 package com.algorithm;
 
+import java.util.Arrays;
+
 /**
  * Created by zgc on 2018/10/27.
+ *
  */
 public class SortTest {
+    public static void main(String[] args) {
+    int[] array = new int[]{9,8,7,4,3,0,1};
+        System.out.println(Arrays.toString(array));
+
+
+
+    }
+
+
+
+
+
 }
